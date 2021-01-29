55c0342fd65560a913bb32c34b91b99626365f69
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2021-01-29 21:51:16 +0900

    wishesテーブル作成

--- db/migrate/20210129123644_create_wishes.rb
+++ db/migrate/20210129123644_create_wishes.rb
@@ -1,7 +1,10 @@
 class CreateWishes < ActiveRecord::Migration[6.0]
   def change
     create_table :wishes do |t|
-
+      t.string :title, null: false
+      t.date :deadline
+      t.text :memo
+      t.boolean :completed, null: false
       t.timestamps
     end
   end
