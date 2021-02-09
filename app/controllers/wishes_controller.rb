cc65062731b6b3fa24af756d3e76bfe311cdbb76
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2021-02-09 11:53:54 +0900

    スモールステップを保存出来るようにした

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -8,7 +8,10 @@ class WishesController < ApplicationController
   end
 
   def create
-    Wish.create(wish_params)
+    wish = Wish.create(wish_params)
+    SmallStep.create(text: small_step_params[:text1], wish_id: wish.id)
+    SmallStep.create(text: small_step_params[:text2], wish_id: wish.id)
+    SmallStep.create(text: small_step_params[:text3], wish_id: wish.id)
   end
 
   def show
@@ -17,8 +20,19 @@ class WishesController < ApplicationController
   private
   def wish_params
     re_params = params.require(:wish).permit(:title, :memo)
+    #deadline以外のデータを取得したハッシュを作成
     date = params.require(:wish).permit(:deadline)
     deadline = Date.new(date["deadline(1i)"].to_i, date["deadline(2i)"].to_i,  date["deadline(3i)"].to_i)
+    #deadlineのデータをDateクラスに変換
     re_params.merge(deadline: deadline)
+    #２つのデータを合体し新しいハッシュを生成
   end
+
+  def small_step_params
+    small_step_params = params.require(:wish).permit(small_step: [:text1,:text2, :text3])
+    # paramsからsmall_stepの情報を取得
+    small_step_params = small_step_params.require(:small_step).permit(:text1, :text2, :text3)
+  end
+
+
 end
