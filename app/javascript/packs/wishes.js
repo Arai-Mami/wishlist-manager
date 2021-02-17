6aa53fa13a4d8b7d539e38e95b355aac929a84c2
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2021-02-17 12:47:42 +0900

    2回目以降jsが効かなくなったのでturbolonks削除

--- app/javascript/packs/wishes.js
+++ app/javascript/packs/wishes.js
@@ -1,8 +1,8 @@
 console.log("Hello world"); // この行が読み込まれた時点で実行される
 // 登録ボタンのDOMを取得
-
 function submitFormWitnButton(){
   var btn = document.getElementById("touroku");
+  console.log(btn)
   function submitForm() {
     console.log("Hello world");
     document.forms.new_wish.submit();
