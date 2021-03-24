c0b085d6d8ae737dcd4db58d60e0dc12b1fbeb3d
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2021-03-24 13:52:55 +0900

    smallstepのdoneの切り替えをsmallStepモデルにメソッドとしてまとめた。(switsh_doneメソッド)

--- app/controllers/small_steps_controller.rb
+++ app/controllers/small_steps_controller.rb
@@ -1,12 +1,6 @@
 class SmallStepsController < ApplicationController
   def update
     small_step = SmallStep.find(params[:id])
-    if small_step.done == false
-      small_step.done = true
-      small_step.save
-    else
-      small_step.done = false
-      small_step.save
-    end
+    small_step.switch_done
   end
 end
