b8f365ea1b3707f2c36a7b3e4554e50d0554854a
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2021-04-22 12:02:32 +0900

    wishesにuser_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_110901) do
+ActiveRecord::Schema.define(version: 2021_04_21_065829) do
 
   create_table "small_steps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "text", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_110901) do
     t.boolean "completed", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
 end
