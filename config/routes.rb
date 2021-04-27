758a185c1994c6aaea719c4af97e8c01a21aa76b
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2021-04-27 18:52:07 +0900

    completeルーティング設定。達成ボタンlink作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :wishes
+  resources :wishes do
+    get :complete
+  end
   resources :dones, only: [:index, :show, :create]
   resources :small_steps, only: [:update]
   root "wishes#index"
